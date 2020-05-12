48d7077626477a23f1dbf30cf92c6f2dd392852e
coreym97 <60191516+coreym97@users.noreply.github.com>
2020-05-12 17:57:58 -0400

    Add variables to handle rotation

--- js/app.js
+++ js/app.js
@@ -2,3 +2,8 @@ let tableElementA = document.getElementsByTagName(`td`)[0];
 let tableElementB = document.getElementsByTagName(`td`)[1];
 let tableElementC = document.getElementsByTagName(`td`)[2];
 let tableElementD = document.getElementsByTagName(`td`)[3];
+let rotatedA = false;
+let rotatedB = false;
+let rotatedC = false;
+let rotatedD = false;
+
