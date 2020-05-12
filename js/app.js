ca1707a5ae3cc99225b7d31f88f992b3dad1d2f6
coreym97 <60191516+coreym97@users.noreply.github.com>
2020-05-12 17:56:08 -0400

    Link td elements to script

---
+++ js/app.js
@@ -0,0 +1,4 @@
+let tableElementA = document.getElementsByTagName(`td`)[0];
+let tableElementB = document.getElementsByTagName(`td`)[1];
+let tableElementC = document.getElementsByTagName(`td`)[2];
+let tableElementD = document.getElementsByTagName(`td`)[3];
