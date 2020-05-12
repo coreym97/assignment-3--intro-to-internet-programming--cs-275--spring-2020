db0d2102f5cf159448e22a3efa6241b7e034aee7
coreym97 <60191516+coreym97@users.noreply.github.com>
2020-05-12 18:01:05 -0400

    Add onclick functions for rotation

--- js/app.js
+++ js/app.js
@@ -7,3 +7,78 @@ let rotatedB = false;
 let rotatedC = false;
 let rotatedD = false;
 
+tableElementA.onclick = () => {
+    if (rotatedB === true) {
+        tableElementA.classList.toggle(`rotated`);
+        tableElementB.classList.toggle(`rotated`);
+        rotatedB = !rotatedB;
+    } else if (rotatedC === true) {
+        tableElementA.classList.toggle(`rotated`);
+        tableElementC.classList.toggle(`rotated`);
+        rotatedC = !rotatedC;
+    } else if (rotatedD === true) {
+        tableElementA.classList.toggle(`rotated`);
+        tableElementD.classList.toggle(`rotated`);
+        rotatedD = !rotatedD;
+    } else {
+        tableElementA.classList.toggle(`rotated`);
+    }
+    rotatedA = !rotatedA;
+};
+
+tableElementB.onclick = () => {
+    if (rotatedA === true) {
+        tableElementB.classList.toggle(`rotated`);
+        tableElementA.classList.toggle(`rotated`);
+        rotatedA = !rotatedA;
+    } else if (rotatedC === true) {
+        tableElementB.classList.toggle(`rotated`);
+        tableElementC.classList.toggle(`rotated`);
+        rotatedC = false;
+    } else if (rotatedD === true) {
+        tableElementB.classList.toggle(`rotated`);
+        tableElementD.classList.toggle(`rotated`);
+        rotatedD = !rotatedD;
+    } else {
+        tableElementB.classList.toggle(`rotated`);
+    }
+    rotatedB = !rotatedB;
+};
+
+tableElementC.onclick = () => {
+    if (rotatedA === true) {
+        tableElementC.classList.toggle(`rotated`);
+        tableElementA.classList.toggle(`rotated`);
+        rotatedA = !rotatedA;
+    } else if (rotatedB === true) {
+        tableElementC.classList.toggle(`rotated`);
+        tableElementB.classList.toggle(`rotated`);
+        rotatedB = !rotatedB;
+    } else if (rotatedD === true) {
+        tableElementD.classList.toggle(`rotated`);
+        tableElementC.classList.toggle(`rotated`);
+        rotatedD = !rotatedD;
+    } else {
+        tableElementC.classList.toggle(`rotated`);
+    }
+    rotatedC = !rotatedC;
+};
+
+tableElementD.onclick = () => {
+    if (rotatedA === true) {
+        tableElementD.classList.toggle(`rotated`);
+        tableElementA.classList.toggle(`rotated`);
+        rotatedA = !rotatedA;
+    } else if (rotatedB === true) {
+        tableElementD.classList.toggle(`rotated`);
+        tableElementB.classList.toggle(`rotated`);
+        rotatedB = !rotatedB;
+    } else if (rotatedC === true) {
+        tableElementD.classList.toggle(`rotated`);
+        tableElementC.classList.toggle(`rotated`);
+        rotatedC = !rotatedC;
+    } else {
+        tableElementD.classList.toggle(`rotated`);
+    }
+    rotatedD = !rotatedD;
+};
